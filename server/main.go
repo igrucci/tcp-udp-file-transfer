3451db9e018b312686b942a108b3d8bf4ff73625
igrucci <babichevigor12@gmail.com>
2024-04-04 10:43:03 +0300

    init

--- server/main.go
+++ server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net"
 	"os"
@@ -63,7 +62,7 @@ func handleUDPConn(udpPort string, tcpConn net.Conn, fileName string) {
 		}
 		data := parts[1]
 
-		fmt.Println("Received udp packet  : id:", packetId, data)
+		log.Println("Received udp packet  : id:", packetId, data)
 
 		_, err = file.WriteString(data)
 		if err != nil {
@@ -73,7 +72,7 @@ func handleUDPConn(udpPort string, tcpConn net.Conn, fileName string) {
 
 		_, err = tcpConn.Write([]byte("received"))
 		if err != nil {
-			fmt.Println("Error writing to tcp", err)
+			log.Println("Error writing to tcp", err)
 			return
 		}
 	}
@@ -83,38 +82,38 @@ func main() {
 
 	l, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		fmt.Println("Error listening on tcp port ", err)
+		log.Println("Error listening on tcp port ", err)
 		return
 	}
 
 	for {
 		tcpConn, err := l.Accept()
 		if err != nil {
-			fmt.Println("Error accepting tcp connection", err)
+			log.Println("Error accepting tcp connection", err)
 			continue
 		}
 		go handleTCPConn(tcpConn)
 
 		defer tcpConn.Close()
-		fmt.Printf("Serving tcp  %s\n", tcpConn.RemoteAddr().String())
+		log.Printf("Serving tcp  %s\n", tcpConn.RemoteAddr().String())
 
 		buf := make([]byte, 1024)
 		n, err := tcpConn.Read(buf)
 		if err != nil {
-			fmt.Println("Error reading from tcp", err)
+			log.Println("Error reading from tcp", err)
 			return
 		}
 		udpPort := string(buf[:4])
 		fileName := string(buf[4:n])
 		if err := os.MkdirAll("files", 0777); err != nil {
-			fmt.Println("Error creating directory:", err)
+			log.Println("Error creating directory:", err)
 			return
 		}
 
 		go handleUDPConn(udpPort, tcpConn, fileName)
 
 		if _, err := os.Create("files/" + fileName); err != nil {
-			fmt.Println("Error creating file:", err)
+			log.Println("Error creating file:", err)
 			return
 		}
 	}
